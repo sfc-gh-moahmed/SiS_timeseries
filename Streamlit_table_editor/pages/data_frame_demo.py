54b9ff172d04a91df0043d0d6b1b65c8ad96c493
Moiz Ahmed <moiz.ahmed@snowflake.com>
2025-05-28 11:00:31 -0400

    Add files via upload

---
+++ Streamlit_table_editor/pages/data_frame_demo.py
@@ -0,0 +1,14 @@
+# Import python packages
+import streamlit as st
+
+
+import pandas as pd
+
+
+# Write directly to the app
+st.title(f"dataframe demo in SiS :balloon:")
+
+df = pd.read_csv('./data/airline-safety.csv')
+
+st.subheader('Dataframe view')
+st.dataframe(df)
